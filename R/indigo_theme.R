2978273092d41ec865a206309269990d70c4196a
indigobannister <indigobannister@ucsb.edu>
2021-02-04 16:44:45 -0800

    initial commit

---
+++ R/indigo_theme.R
@@ -0,0 +1,16 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+indigo_theme <- function() {
+  theme(
+    panel.background = element_rect(fill = "green"),
+    panel.grid.major.x = element_line(colour = "green", linetype = 3, size = 0.5),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y =  element_line(colour = "yellow", linetype = 3, size = 0.5),
+    axis.text = element_text(colour = "red"),
+    axis.title = element_text(colour = "orange")
+  )
+}
